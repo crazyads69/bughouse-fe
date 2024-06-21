439f15b32de39cccebaad6dd979639fea8c46ea9
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-06-21 22:13:38 +0700

    Update notify

--- src/utils/show-noti.tsx
+++ src/utils/show-noti.tsx
@@ -11,9 +11,26 @@ class Notify {
 	warning = (message: string) => toast.warning(<Noti message={message} />)
 }
 
+/**
+ * Represents a class that provides different types of notifications.
+ */
 class NotifyType {
+	/**
+	 * Displays a success notification with the specified message.
+	 * @param message - The message to be displayed in the notification.
+	 */
 	success = (message: string) => toast.success(<Noti message={message} />)
+
+	/**
+	 * Displays an error notification with the specified message.
+	 * @param message - The message to be displayed in the notification.
+	 */
 	error = (message: string) => toast.error(<Noti message={message} />)
+
+	/**
+	 * Displays a warning notification with the specified message.
+	 * @param message - The message to be displayed in the notification.
+	 */
 	warning = (message: string) => toast.warning(<Noti message={message} />)
 }
 
