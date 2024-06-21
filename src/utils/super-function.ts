c1ba0dd24a89a7f64cabd0f35a8f09cde9ceca92
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-06-21 22:14:24 +0700

    update super function code

--- src/utils/super-function.ts
+++ src/utils/super-function.ts
@@ -1,7 +1,20 @@
+/**
+ * Encodes a string using base64 encoding.
+ *
+ * @param str - The string to encode.
+ * @returns The encoded string.
+ */
 export function encode(str: string) {
 	return window?.btoa(unescape(encodeURIComponent(str)))
 }
 
+/**
+ * Decodes a string by performing base64 decoding and URI decoding.
+ * If the input string is empty, 'undefined', or contains a '+', an empty string is returned.
+ *
+ * @param str - The string to decode.
+ * @returns The decoded string.
+ */
 export function decode(str: string) {
 	if (!str || str == 'undefined' || str.includes('+')) {
 		return ''
